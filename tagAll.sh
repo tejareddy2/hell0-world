4d4b874c97db181e0bf9d71d56d5b84c93e22dc5
teja <suddhatejeswar.reddy@altran.com>
2019-09-25 18:23:34 +0530

    wfEF

--- tagAll.sh
+++ tagAll.sh
@@ -1,9 +1,3 @@
-red=$(tput setaf 1)
-green=$(tput setaf 2)
-bold=$(tput bold)
-reset=$(tput sgr0)
-declare -a imagesFilter
-
 while [ -n "$1" ]; do # while loop starts
   case "$1" in
   --front)
@@ -13,6 +7,18 @@ while [ -n "$1" ]; do # while loop starts
     imagesFilter[1]=data
     ;;
   --tools)
+    imagesFilter[2]=tools
+    ;;
+  --data,front | --front,data)
+    imagesFilter[0]=front
+    imagesFilter[1]=data
+    ;;
+  --tools,front | --front,tools)
+    imagesFilter[0]=front
+    imagesFilter[2]=tools
+    ;;
+  --tools,data | --data,tools)
+    imagesFilter[1]=data
     imagesFilter[2]=tools
     ;;
   -f | --from)
@@ -54,7 +60,17 @@ while [ -n "$1" ]; do # while loop starts
   esac
   shift
 done
+
+# If no filter is passed, we plan to use all images (so wildward used).
+# shellcheck disable=SC2128
 if [ ${#imagesFilter[@]} == 0 ]; then
   imagesFilter[0]="*"
 fi
+
+# If no profile is passed or guessed via the .env file content don't pass a profile (rely on EC2 IAM roles or default profile).
+AWS_PROFILE_CLI=""
+if [ -n "$awsProfile" ]; then
+  AWS_PROFILE_CLI="--profile $awsProfile"
+fi
+
 echo -e "AWS ECR Repository Tagging\n--------------------------\nImages:      ${green}${imagesFilter[*]}${reset}\nTag From:    ${green}$tagFrom${reset}\nTag To:      ${green}$tagTo${reset}\nAWS Profile: ${green}$awsProfile${reset}\n"
